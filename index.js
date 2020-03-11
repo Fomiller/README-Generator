55d61d2fb1cf8cf5471c4e4028d2b2a84bf1d732
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-11 18:03:31 -0500

    api method working

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-var fs = require("fs");
-var axios = require("axios");
-var inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const api = require("./utils/api.js");
 
-const questions = [
-    
-];
+const userName= "Fomiller";
 
+api.getUser(userName)
 
+// const questions = [
+//     {
+//         type: "input",
+//         message: "What is your user name?",
+//         name: "username"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your first name?",
+//         name: "firstName"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your last name?",
+//         name: "lastName"
+//     },
+// ];
 
-
-
-
-
-inquirer
-    .prompt([
-        {
-        type: "input",
-        message: "What is your user name?",
-        name: "username"
-        },
-    ])
-    .then(function(response) {
-
-        if (response.confirm === response.password) {
-          console.log("Success!");
-        }
-        else {
-          console.log("You forgot your password already?!");
-        }
-      });
+// inquirer
+//     .prompt(questions)
+//     .then(function(response) {
+//         console.log(response.username)
+//         console.log(response.firstName)
+//         console.log(response.lastName)
+//       });
 
 
 function writeToFile(fileName, data) {
--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,37 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
+
 const api = {
-    getUser(username) {
-  
+    getUser({ username }){
+
+      inquirer
+      .prompt({
+        message: "Enter your GitHub username",
+        name: "username"
+      })
+      .then(function({ username }){
+        const queryUrl = `https://api.github.com/users/${username}`;
+            axios
+              .get(queryUrl)
+              .then(function(res) {
+                console.log(res)
+                // console.log(res.data)
+                // console.log(res.data.avatar_url)
+                // console.log(res.data.email)
+              })
+              .catch(function (error) {
+              // handle error
+              console.log(error);
+              })
+        });  
     }
-  };
-  
-  module.exports = api;
+};
+    
+
+
+
+module.exports = api;
   
