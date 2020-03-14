17ab871706da9af7a3ff9b3b4f6d830374aeffad
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-13 21:57:18 -0500

    Generating a markdown file with correct inputs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,56 @@
 function generateMarkdown(data) {
-    return `
-  # ${data.title}
-  
-  `;
-  }
-  
-  module.exports = generateMarkdown;
-  
+const { gitData } = data;
+const { questionData } = data;
+
+const createTable = function(data) {
+  const array2 = data.map(element => `* ${element}`)
+  var table = array2.join("\n")
+  return table
+};
+
+return `
+# ${questionData.title}
+## Description
+${data.description}
+
+## Table of Contents
+${createTable(questionData.contents)}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.License}
+
+## Contributing
+${data.contribution}
+
+`;
+};
+
+module.exports = {generateMarkdown};
+
+'Installation',
+'Usage',
+'Contributing',
+'License',
+'Credits',
+'Badges',
+'Test',
+'Contact'
+// # ${data.title}
+// ## ${gitData.login}
+// ### ${gitData.blog}
+
+// ## Table of contents
+// ## Credits
+// * ${data.Credits[0]}
+// * ${data.Credits[1]}
+// * ${data.Credits[2]}
+// ## languages
+// * ${data.badges}
+// * ${data.badges}
+// * ${data.badges}
